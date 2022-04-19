653d0ee972ad4961252d4aaca874c4dce5a6151b
molteni <molteni@et.intechinfo.fr>
2022-04-19 11:44:20 +0200

    new branche

---
+++ log.js
@@ -0,0 +1,12 @@
+console.log("logs");
+
+try {
+var a = true;
+    if (a) {
+        console.log('a')
+    }else {
+        throw new Error("log");
+    }
+}   catch(e){
+        throw new Error("zigoums il baise des vieilles a la chatte fripé")
+}
--- omar.js
+++
@@ -1,1 +0,0 @@
-console.log("omar pu des pieds")
